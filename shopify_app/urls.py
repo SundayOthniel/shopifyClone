d628c53901f5e94c8b914a401bc0b96c15dbdaf8
Sunday Othniel <Sundayothniel@outlook.com>
2025-08-02 09:54:25 +0100

    Update urls.py

--- shopify_app/urls.py
+++ shopify_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from shopify_app import views
 
 urlpatterns = [
-    path('shopify', views.home, name='home'),
+    path('', views.home, name='home'),
     path('shopify/select-plan', views.middle, name='plans'),
     path('shopify/checkout', views.payment, name='checkout')
 ]
